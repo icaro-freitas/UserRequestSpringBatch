059afbfb4c2dd58118633134062d2bedb5a28f33
icaro-freitas <icaro_freitas2014@outlook.com>
2024-11-26 18:14:25 -0300

    Processor added to step

--- src/main/java/com/devsuperior/user_request_sb/step/FetchUserDataAndStoreDBStepConfig.java
+++ src/main/java/com/devsuperior/user_request_sb/step/FetchUserDataAndStoreDBStepConfig.java
@@ -3,6 +3,7 @@ package com.devsuperior.user_request_sb.step;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import com.devsuperior.user_request_sb.dto.UserDTO;
+import com.devsuperior.user_request_sb.entities.User;
 
 @Configuration
 public class FetchUserDataAndStoreDBStepConfig {
@@ -24,12 +26,14 @@ public class FetchUserDataAndStoreDBStepConfig {
 
 	@Bean
 	public Step fetchUserDataAndStoreDBStep(ItemReader<UserDTO> fetchUserDataReader, 
-			ItemWriter<UserDTO> inserUserDataDBWriter,
+			ItemProcessor<UserDTO, User> selectFieldsUserDataProcessor,
+			ItemWriter<User> inserUserDataDBWriter,
 			JobRepository jobRepository) {
 		
 		return new StepBuilder("fetchUserDataAndStoreDBStep", jobRepository)
-				.<UserDTO, UserDTO>chunk(chunkSize, transactionManager)
+				.<UserDTO, User>chunk(chunkSize, transactionManager)
 				.reader(fetchUserDataReader)
+				.processor(selectFieldsUserDataProcessor)
 				.writer(inserUserDataDBWriter)
 				.build();
 	}
