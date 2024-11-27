3d1ada05dd24ce86b918d6e6ffa6a507545fddc3
icaro-freitas <icaro_freitas2014@outlook.com>
2024-11-26 21:47:53 -0300

    Configure multiple databases

--- src/main/java/com/devsuperior/user_request_sb/step/FetchUserDataAndStoreDBStepConfig.java
+++ src/main/java/com/devsuperior/user_request_sb/step/FetchUserDataAndStoreDBStepConfig.java
@@ -7,6 +7,7 @@ import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,6 +20,7 @@ import com.devsuperior.user_request_sb.entities.User;
 public class FetchUserDataAndStoreDBStepConfig {
 
 	@Autowired
+	@Qualifier("transactionManagerApp")
 	private PlatformTransactionManager transactionManager;
 
 	@Value("${chunkSize}")
